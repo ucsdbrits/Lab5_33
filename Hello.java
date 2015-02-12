e578f3ed5a8fe0bbc35ba4afacb4eeea51dfd4c3
Rica <rhc006@ucsd.edu>
2015-02-12 15:53:39 -0800

    Add an author comment

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello
     public static void main( String argv[] )
     {
 	// Default is "World"
+	// Author: Brittany Spears
         String name = "World";
         if ( argv.length != 0 )
         {
